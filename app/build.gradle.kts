4b843939ac593409a3b9a242542fd44d7039a9d3
markd <mark@veteranop.com>
2026-03-31 13:13:51 -0600

    chore: release 1.0.1.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         applicationId = "com.veteranop.cellfire"
         minSdk = 26
         targetSdk = 35
-        versionCode = 14
-        versionName = "1.0.1.4"
+        versionCode = 15
+        versionName = "1.0.1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
